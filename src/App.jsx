57ef0b1df5bc0a8351af0823918ba72059184054
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-02-22 10:26:24 +0530

    Starting...

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserInput />
+      {/* Result Go Here */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>Result...</div> // this is for temporary. we can replace it later.
+  )
+}
+
+export default Result;
+
+// in this components we calculate result data.
